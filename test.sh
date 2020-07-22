b79b573df44429c61edc72b3f0200042be67a37f
Kali <kali@kali>
2020-07-22 17:35:13 -0400

    tesh.sh created

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "ENter your NAme: " name
+read -p "Number of Directories to Create: " n
+
+a=0
+
+until [ $a -eq $n ]
+do
+	cd
+	cd Desktop
+	a=`expr $a + 1`
+	mkdir $name$a
+done
+
+echo "DOne ! Go & take a look of Desktop :D"
